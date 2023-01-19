19337f63443a2bb3934f4a807e0d4143aabf948c
root <root@rdr-varad-412-bastion-0.rdr-varad-412.ibm.com>
2023-01-19 06:59:41 -0500

    am

--- hack/ocp-e2e-tests-handler.sh
+++ hack/ocp-e2e-tests-handler.sh
@@ -14,13 +14,10 @@ export KUBEVIRT_PROVIDER=external
 export IMAGE_BUILDER="${IMAGE_BUILDER:-podman}"
 export DEV_IMAGE_REGISTRY="${DEV_IMAGE_REGISTRY:-quay.io}"
 export KUBEVIRTCI_RUNTIME="${KUBEVIRTCI_RUNTIME:-podman}"
-export PRIMARY_NIC=enp2s0
-export FIRST_SECONDARY_NIC=enp3s0
-export SECOND_SECONDARY_NIC=enp4s0
 export FLAKE_ATTEMPTS="${FLAKE_ATTEMPTS:-5}"
 
 SKIPPED_TESTS="user-guide|bridged|\
-when desiredState is updated with ovs-bridge with linux bond as port" # https://bugzilla.redhat.com/show_bug.cgi?id=2005240 is not yet fixed in nmstate 1.2
+when desiredState is updated with ovs-bridge with linux bond as port" # https://bugzilla.redhat.com/show_bug.cgi?id=2005240 is not yet fixed in openshift-nmstate 1.2
 
 if [ "${CI}" == "true" ]; then
     source ${SHARED_DIR}/fix-uid.sh
@@ -51,12 +48,10 @@ if oc create -f test/e2e/machineconfigs.yaml; then
 fi
 while ! oc wait mcp --all --for=condition=Updated --timeout -1s; do sleep 1; done
 
-make cluster-sync-operator
-oc create -f test/e2e/nmstate.yaml
 # On first deployment, it can take a while for all of the pods to come up
 # First wait for the handler pods to be created
-while ! oc get pods -n nmstate | grep handler; do sleep 1; done
+while ! oc get pods -n openshift-nmstate | grep handler; do sleep 1; done
 # Then wait for them to be ready
-while oc get pods -n nmstate | grep "0/1"; do sleep 1; done
+while oc get pods -n openshift-nmstate | grep "0/1"; do sleep 1; done
 # NOTE(bnemec): The test being filtered with "bridged" was re-enabled in 4.8, but seems to be consistently failing on OCP.
 make test-e2e-handler E2E_TEST_ARGS="--skip=\"${SKIPPED_TESTS}\" --flake-attempts=${FLAKE_ATTEMPTS}" E2E_TEST_TIMEOUT=4h
